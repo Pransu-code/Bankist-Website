0ff293d35996496caf8f448438719f694d52a2bc
Pransu-code <pransu54321@gmail.com>
2024-07-26 01:48:07 +0530

    Added lazy-loading img&content feature

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+const tabsContent = document.querySelectorAll('.operations__content');
+
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -53,11 +60,6 @@ document.querySelector('.nav__links').addEventListener('click',function(e){
   }
 });
 
-const tabs = document.querySelectorAll('.operations__tab');
-
-const tabsContainer = document.querySelector('.operations__tab-container');
-
-const tabsContent = document.querySelectorAll('.operations__content');
 
 
 tabsContainer.addEventListener('click',function(e){
@@ -70,6 +72,7 @@ tabsContainer.addEventListener('click',function(e){
   // }
 
   const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
   if(!clicked) return;
   //active tab
   tabs.forEach(tb => tb.classList.remove('operations__tab--active'));
@@ -83,3 +86,89 @@ tabsContainer.addEventListener('click',function(e){
 
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
+
+const handleHover = function(e){
+  // console.log(e.target);
+  // console.log(this);
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if(el!==link) el.style.opacity = this;
+    });
+
+    logo.style.opacity = this;
+  }
+}
+// we can do this
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout',handleHover.bind(1));
+
+// nav.addEventListener('mouseout', function(e){
+  // handleHover(e,1)});
+
+  const initialChords = section1.getBoundingClientRect();
+
+  window.addEventListener('scroll',function(e){
+    // if(window.scrollY>initialChords.top){
+      nav.classList.add('sticky');
+  //   }
+  //   else{
+  //     nav.classList.remove('sticky');
+  //   }
+  })
+
+  //intersection of server api for same sticky navigation
+
+  const allSections = document.querySelectorAll('.section');
+
+  const revealSection = function(entries,observer){
+    const [entry] = entries;
+
+    if(!entry.isIntersecting){
+    entry.target.classList.add('section--hidden');
+    }else{
+    entry.target.classList.remove('section--hidden');
+    }
+ 
+  };
+
+
+  const sectionObserver = new IntersectionObserver(revealSection, {
+    root : null,
+    threshold : 0.15,
+    // rootMargin : `-`/
+
+  })
+
+  allSections.forEach(function(section){
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+  })
+
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries,observer){
+  const[entry] = entries;
+
+  if(!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load',function(){
+  
+    entry.target.classList.remove('lazy-img');
+  
+  });
+
+  observer.unobserve(entry.target);
+}
+const imgObserver = new IntersectionObserver(loadImg, {
+  root : null,
+  threshold : 0,
+  rootMargin: '-200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
