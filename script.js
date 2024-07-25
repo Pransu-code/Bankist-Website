734f9361159c0346ad4a625d8cf79ed2029f0b30
Pransu-code <pransu54321@gmail.com>
2024-07-25 22:10:21 +0530

    Added smooth scroll and event delegations

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -18,9 +20,12 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click',openModal));
 
+// btnsOpenModal.addEventListener('click',function(){
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// });
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -28,4 +33,53 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+//scroll button
+
+btnScrollTo.addEventListener('click',function(){
+
+  section1.scrollIntoView({behavior : 'smooth'
+  });
+})
+
+document.querySelector('.nav__links').addEventListener('click',function(e){
+  e.preventDefault();
+  
+  if(e.target.classList.contains('nav__link')){
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior:'smooth'
+    });
+  }
+});
+
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+const tabsContent = document.querySelectorAll('.operations__content');
+
+
+tabsContainer.addEventListener('click',function(e){
+  e.preventDefault();
+  // console.log(e.target);
+  // if(e.target.classList.contains('operations__tab')){
+  //   const id = e.target.getAttribute('data-tab');
+  //   console.log(id);
+  //   document.querySelector.add(`btn operations__tab operations__tab--${id}`);
+  // }
+
+  const clicked = e.target.closest('.operations__tab');
+  if(!clicked) return;
+  //active tab
+  tabs.forEach(tb => tb.classList.remove('operations__tab--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  //remove tabs
+  tabsContent.forEach(tc => tc.classList.remove('operations__content--active'));
+
+  //activate content area
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
